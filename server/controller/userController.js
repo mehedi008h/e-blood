26c75ab347cce47d263aa69f362dde27ba426cc1
Mehedi Hasan <mehedi08h@gmail.com>
2022-04-17 00:11:32 +0600

    update profile & password

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken");
 
 // Get currently logged in user details   =>   /api/v1/me
 exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
@@ -10,5 +11,40 @@ exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
         success: true,
         user,
+    });
+});
+
+// Update / Change password   =>  /api/v1/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select("+password");
+
+    // Check previous user password
+    const isMatched = await user.comparePassword(req.body.oldPassword);
+    if (!isMatched) {
+        return next(new ErrorHandler("Old password is incorrect"));
+    }
+
+    user.password = req.body.password;
+    await user.save();
+
+    sendToken(user, 200, res);
+});
+
+// Update user profile   =>   /api/v1/me/update
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        address: req.body.address,
+        phone: req.body.phone,
+    };
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
     });
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getUserProfile } = require("../controller/userController");
+const {
+    getUserProfile,
+    updatePassword,
+    updateProfile,
+} = require("../controller/userController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
 
 // user
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 
 module.exports = router;
